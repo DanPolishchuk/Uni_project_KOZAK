7c16589b77093e9c0b2f76a2c91bf7cc95ba8702
DanPolishchuk <109544766+DanPolishchuk@users.noreply.github.com>
2023-02-03 15:20:14 +0100

    Update telegram_credentials.py

--- telegram_credentials.py
+++ telegram_credentials.py
@@ -1,9 +1,9 @@
 from telethon import TelegramClient
 
-api_id = 29394025                               # api_id and api_hash are mandatory values for connecting to Telegram
+api_id = "your api id"                               # api_id and api_hash are mandatory values for connecting to Telegram
 
-api_hash = "7019b119189bb89c7cafa94020981fe4"   # client, you can get them by logging in on www.my.telegram.org
+api_hash = "your api hash"   # client, you can get them by logging in on www.my.telegram.org
 
-client = TelegramClient("DeadHeadOG", api_id, api_hash)  # here we specify to which Telegram client we want to connect
+client = TelegramClient("your account", api_id, api_hash)  # here we specify to which Telegram client we want to connect
 
 
