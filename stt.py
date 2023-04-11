127529ff3bc55dcecf08a356b632df4ee06afb03
DanPolishchuk <109544766+DanPolishchuk@users.noreply.github.com>
2023-04-11 21:52:00 +0200

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,15 +1,28 @@
 from vosk import KaldiRecognizer, Model, SetLogLevel
+from torch.hub import download_url_to_file
 from sounddevice import RawInputStream
 from queue import Queue
 from json import loads
+from os.path import isfile, splitext
+from zipfile import ZipFile
 
-SetLogLevel(-1)
-model = Model("path to your model")                   # here we assign a language model for speech recognition
+SetLogLevel(-1)                                                                 
 q = Queue()
+
+model_check = "vosk-model-uk-v3\\vosk-model-uk-v3\\am\\final.mdl"
+model_file_zip = "vosk-model-uk-v3.zip"
+
+if not isfile(model_check):
+    download_url_to_file('https://alphacephei.com/vosk/models/vosk-model-uk-v3.zip', model_file_zip)
+    
+    with ZipFile(model_file_zip, "r") as f:
+        f.extractall(splitext(model_file_zip)[0])
+        
+model = Model("vosk-model-uk-v3\\vosk-model-uk-v3")
 
 
 def q_callback(indata, frames, time, status):
-    q.put(bytes(indata))                                        # creates the data to be read
+    q.put(bytes(indata))
 
 
 def kozak_listen(callback):
@@ -21,5 +34,5 @@ def kozak_listen(callback):
             if len(data) == 0:
                 break
             if rec.AcceptWaveform(data):
-                callback(loads(rec.Result())["text"])                # returns results to callback function
+                    callback(loads(rec.Result())["text"])
 
